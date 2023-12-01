97f1ffed5a0d81bc01e12ff8d731e08a18321fd2
xiansakana <2461298052@qq.com>
2023-12-01 04:39:24 -0500

    update invoice page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from 'next';
+
 export default function Page() {
   return <p>Custormers Page</p>;
 }
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+import Pagination from '@/app/ui/invoices/pagination';
